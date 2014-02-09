5a22cf6c5a6c9392851673faf0bf25a5d2bfdcfb
Diego Mijelshon <diego@mijelshon.com.ar>
2014-02-09 20:31:34 -0300

    Link Facebook en footer

--- foot.php
+++ foot.php
@@ -1,4 +1,8 @@
 <div class="footer">
     <div class="container">
+        Seguinos:
+        <a href="https://www.facebook.com/EscuelayComunidadTelAviv">
+            <img src="FB-f-Logo__blue_29.png" alt="Facebook">
+        </a>
     </div>
 </div>
