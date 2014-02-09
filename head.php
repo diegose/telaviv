e3e8ac6a5ee0d5dcbead1c810babcf1c1f8a8c43
Diego Mijelshon <diego@mijelshon.com.ar>
2014-02-09 19:28:39 -0300

    Actualización de Bootstrap y jQuery. Protocolo dinámico.

--- head.php
+++ head.php
@@ -1,6 +1,6 @@
     	<meta charset="utf-8"> 
     	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
 		<link rel="stylesheet" href="telaviv.css">
-		<script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
-		<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+		<script src="//code.jquery.com/jquery-2.1.0.min.js"></script>
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
